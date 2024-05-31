371cac711a10a5d350f5619447c77269615a30a5
Ubuntu <ubuntu@ip-172-31-44-92.eu-west-1.compute.internal>
2024-05-31 08:44:15 +0000

    jenkins

--- proyectos/proyecto/src/main/java/com/curso/Libreria.java
+++ proyectos/proyecto/src/main/java/com/curso/Libreria.java
@@ -3,7 +3,7 @@ package com.curso;
 public interface Libreria {
     
     public static double sumar(double numero1, double numero2){
-        return numero1 + numero2;
+        return numero1 - numero2;
     }
 
     public static double doblar(double numero){
